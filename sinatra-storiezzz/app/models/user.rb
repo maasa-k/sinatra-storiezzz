34a05e6bb35bfef7e2e7441098d8d464edfead32
maasa333 <konomaasa@gmail.com>
2019-10-13 12:07:54 -0700

    Complete assessment specifications

--- sinatra-storiezzz/app/models/user.rb
+++ sinatra-storiezzz/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
     has_many :dreams
-    validates :username, :email, presence: true
+    validates :username, :email, presence: true, uniqueness: true
     has_secure_password
 end
